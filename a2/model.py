backprop fixed

a2/model.py
@@ -3,51 +3,56 @@ import numpy as np
 
 class LinearModel:
     def __init__(self, d1, d2, K, act_func, loss_func, grad_lossfunc):
+        self.in_size = d1
+        self.hidden_size = d2
+        self.K = K
         self.w_h = np.random.randn(d1, d2) * 2 / (d1 + d2)
-        self.v_wh = np.ones(d1, d2) * 0.00001
+        self.v_wh = np.ones((d1, d2)) * 0.00001
         self.w_o = np.random.randn(d2, K) * 2 / (d2 + K)
-        self.v_wo = np.ones(d2, K) * 0.00001
+        self.v_wo = np.ones((d2, K)) * 0.00001
         self.b_h = np.random.randn(d2, 1) * 2 / (d2 + 1)
-        self.v_bh = np.ones(d2, 1) * 0.00001
-        self.b_o = np.random.randn()
+        self.v_bh = np.ones((d2, 1)) * 0.00001
+        self.b_o = np.random.randn(K, 1) * 2 / (K + 1)
         self.v_bo = 0.00001
         self.act_func = act_func
         self.loss_func = loss_func
         self.grad_lossfunc = grad_lossfunc
         self.loss = 0
+        self.x = None
         self.x_h = None
         self.x_h_a = None
         self.x_o = None
         self.x_o_a = None
 
-    def grad_linear(self, x):
-        return x
 
-    def grad_relu(x):
+    def grad_relu(self, x):
         return (x > 0).astype(int)
 
     def forward(self, x):
-        self.x_h += np.matmul(self.w_h.T, x) + self.b_h
-        self.x_h_a = self.act_func[0](self.x_h)
-        self.x_o += np.matmul(self.w_o.T, self.outputs[-1]) + self.b_o
-        self.x_o_a = self.act_func[1](self.x_o)
-        return self.x_o_a
+        x = x.reshape(-1, 1)
+        self.x=x
+        self.x_h = np.matmul(self.w_h.T, x) + self.b_h  # shape d2 * 1
+        self.x_h_a = self.act_func[0](self.x_h)  # shape d2 * 1
+        self.x_o = np.matmul(self.w_o.T, self.x_h_a) + self.b_o  # shape K * 1
+        self.x_o_a = self.act_func[1](self.x_o)  # shape K * 1
+        self.x_o_a.reshape(-1)  # shape K,
+        return (self.x_o_a+0.00000000001)
 
     def compute_loss(self, target, x):
         self.loss = self.loss_func(target, x)
         return self.loss
 
-    def loss_backward(self, gamma=0.99, alpha=0.00001):
-        grad_loss = self.grad_lossfunc(self.loss, self.x_o_a)
-        grad_wo = grad_loss * self.x_o
+    def loss_backward(self, target, gamma=0.99, alpha=0.00001):
+        grad_loss = np.array(self.grad_lossfunc(target, self.x_o_a.reshape(-1))).reshape(-1, 1)  # shape K * 1
+        grad_wo = np.matmul(grad_loss, self.x_h.T).T  # shape d2 * K
         grad_bo = grad_loss
-        grad_wh = grad_loss * self.grad_linear(self.x_o_a) * self.grad_relu(self.x_h) * self.x_h
-        grad_bo = grad_loss * self.grad_linear(self.x_o_a) * self.grad_relu(self.x_h)
+        grad_wh = np.matmul(self.x,self.grad_relu(self.x_h).T*np.matmul(self.w_o,grad_loss).T)
+        grad_bh = self.grad_relu(self.x_h)*np.matmul(self.w_o,grad_loss)
         self.v_wo = gamma * self.v_wo + alpha * grad_wo
         self.w_o -= self.v_wo
         self.v_bo = gamma * self.v_bo + alpha * grad_bo
         self.b_o -= self.v_bo
         self.v_wh = gamma * self.v_wh + alpha * grad_wh
         self.w_h -= self.v_wh
-        self.v_wo = gamma * self.v_wo + alpha * grad_wo
-        self.w_o -= self.v_wo
+        self.v_bh = gamma * self.v_bh + alpha * grad_bh
+        self.b_h -= self.v_bh

a2/starter.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import os
-from model import linearModel
+from model import LinearModel
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
@@ -73,7 +73,10 @@ def computeLayer(x, W, b):
 def CE(target, prediction):
 
     # Cross Entropy loss for target and prediction
-    loss = (-1 / target.shape[0]) * np.sum(target * np.log(prediction))
+    loss = (-1 / target.shape[0]) * np.sum(target[np.where(target==1)] * np.log(prediction[np.where(target==1)]))
+    if np.isnan(loss) or np.isinf(loss):
+        print(prediction,target)
+        1/0
     return loss
 
 
@@ -143,19 +146,30 @@ def train_torch_model(lr = 0.0001, epoch = 50):
             optimizer.step()
 
 
-
-
-
+def train_numpy_model(hidden_dim,epoch=200):
+    trainData, validData, testData, trainTarget, validTarget, testTarget=loadData()
+    trainTarget,validTarget,testTarget=convertOneHot(trainTarget,validTarget,testTarget)
+    model = LinearModel(trainData.shape[1]*trainData.shape[2], hidden_dim, 10, [relu, softmax], CE, gradCE)
+    acc=np.zeros((epoch,trainData.shape[0]))
+    for i in range(epoch):
+        loss=0
+        for j in range(trainData.shape[0]):
+            input=trainData[j].reshape(-1,)
+            label=trainTarget[j]
+            prediction=model.forward(input)
+            #print(prediction.reshape(-1,))
+            model.loss_backward(label)
+            loss+=model.compute_loss(trainTarget[j], prediction)
+        print(i,loss/trainData.shape[0])
 
 
 if __name__ == "__main__":
-    d1 = #input size
-    d2 = [100,500,2000]
+    d2 = [2000]
+    epoch=100
+
     for hidden_size in d2:
-        m = linearModel(d1, hidden_size, 10, [relu,softmax], CE, gradCE)
-        #training stuff
+        train_numpy_model(hidden_size)
 
     y = np.random.random((5,))
     y_hat = np.random.random((5,))
-
     train_torch_model()